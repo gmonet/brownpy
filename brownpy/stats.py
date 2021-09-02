1297fe7f30c44b8322ae3ea59448313f4082fd02
Geoffrey Monet <monet_geoffrey@hotmail.fr>
2021-09-02 08:28:58 +0200

    MRV

--- brownpy/stats.py
+++ brownpy/stats.py
@@ -1,5 +1,7 @@
 import cupy as cp
 import numpy as np
+from numba import jit
+import numba as nb
 
 def PSD_gpu(x, dt):
     N = len(x)
@@ -25,3 +27,55 @@ def PSD_gpu_binned(x, dt, bins):
     del x_gpu, psd_gpu, freqs_gpu, psd_binned_gpu, freqs_binned_gpu, N_binned_gpu
     cp._default_memory_pool.free_all_blocks()
     return psd_averaged, np.exp(freqs)
+
+# https://jonisalonen.com/2014/efficient-and-accurate-rolling-standard-deviation/
+# https://www.dfki.de/fileadmin/user_upload/import/7051_131008_Memory_and_Processing_Efficient_Formula_for_Moving_Variance_Calculation_in_EEG_and_EMG_Signal_Processing_NEUROTECHNIX_Krell.pdf
+
+@jit(nopython=True)
+def _compute_MRV(x, n, ddof=0):
+    '''
+    Compute Mean Rolling Variance (MRV)
+
+    Attributes:
+        x (ndarray): Description of `attr1`.
+        n (int): Window size
+        ddof(int, optional): Means Delta Degrees of Freedom.  The divisor used in calculations
+            is ``N - ddof``, where ``N`` represents the number of elements.
+            By default `ddof` is zero.
+    '''
+    N = len(x)
+    if n > N:
+        return np.var(x)
+    
+    div = n-ddof
+    mn = np.sum(x[0:n])
+    Sn = n**2*np.var(x[0:n])
+    sum_Sn = Sn
+    for i_0 in range(N-n):
+        i_n = i_0 + n
+        i_n = i_0 + n
+        x_0, x_n = x[i_0], x[i_n]
+        Sn = Sn + (x_n-x_0)*((n-1)*x_n + (n+1)*x_0 - 2*mn)
+        mn = mn + (x_n-x_0)
+        sum_Sn += Sn
+    return sum_Sn/n/div/(N-n+1)
+
+@jit(nopython=True, parallel=True)
+def compute_MRV(x, windows, ddof=0):
+    '''
+    Compute Mean Rolling Variance (MRV)
+
+    Attributes:
+        x (ndarray): Description of `attr1`.
+        intervals (ndarray): List of window size
+        ddof(int, optional): Means Delta Degrees of Freedom.  The divisor used in calculations
+            is ``N - ddof``, where ``N`` represents the number of elements.
+            By default `ddof` is zero.
+
+    '''
+    N_int = len(windows)
+    result = np.empty(N_int)
+    for i in nb.prange(N_int):
+        result[i] = _compute_MRV(x, windows[i], ddof)
+    
+    return result
