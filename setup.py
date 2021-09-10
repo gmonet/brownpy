8ca6e5f4ede36371d8cc8a4bbe8cbb63eeb6d21e
Geoffrey Monet <monet_geoffrey@hotmail.fr>
2021-09-10 15:50:50 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ from setuptools import setup, find_packages
 
 setup(
     name='brownpy',
-    version='0.2.0b',
+    version='0.2.1b',
     description='Brownian motion through nanochannel',
     author='Geoffrey Monet',
-    packages=find_packages(include=['channel-bm', 'channel-bm.*']),
+    packages=find_packages(),
     python_requires='>=3.7, <4',
     install_requires=[
         'numpy>=1.16.5',
         'scipy>=1.3.1',
-        'matplotlib>=3.1.1',
-        'numba>=0.45.1',
-        'tqdm>=4.36.1',
-        'netCDF4>=3',
+        'matplotlib>=3',
+        'numba>=0.45',
+        'tqdm>=4',
+        'h5py>=2',
     ],
     include_package_data=True,
 )
